aa4c0d52cc915ea81f6981659bac0dd06bdae273
Matt Trifilo <matt.trifilo@gmail.com>
2017-06-06 13:21:07 -0500

    solve exponent functions

--- recursion/solved/recursionIntro.js
+++ recursion/solved/recursionIntro.js
@@ -23,11 +23,38 @@ function recurseToZero(n) {
   console.log(n);
   console.log("done!");
 }
-recurseToZero(10);
+// recurseToZero(10);
 
 //3.Write a function 'exponent' that takes two arguments base, and expo, uses a while loop to return the exponenet value of the base.
+function exponent(base, expo) {
+  if (typeof base !== "number" || typeof expo !== "number") {
+    return "base and expo must be numbers";
+  }
+
+  let result = base;
+
+  while (expo > 1) {
+    result *= base;
+    --expo;
+  }
+  console.log("result:", result);
+}
+// exponent(3, 3);
 
 //4. Write a function 'RecursiveExponent' that takes two arguments base, and expo, recursively returns exponent value of the base.
+function recursiveExponent(base, expo) {
+  if (typeof base !== "number" || typeof expo !== "number") {
+    return "base and expo must be numbers";
+  }
+
+  if (expo > 1) {
+    return base * recursiveExponent(base, --expo);
+  }
+  return base;
+}
+
+const exponentResult = recursiveExponent(3, 3);
+console.log("exponentResult:", exponentResult);
 
 //5. Write a function 'recursiveMultiplier' that takes two arguments, 'arr and num', and multiplies each arr value into by num and returns an array of the values.
 
