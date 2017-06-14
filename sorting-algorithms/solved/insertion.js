2e41be0e8c7455cf1d9615fa9b18911b4ab7b959
Matt Trifilo <matt.trifilo@gmail.com>
2017-06-14 17:10:59 -0500

    fix inner loop comparison operator

--- sorting-algorithms/solved/insertion.js
+++ sorting-algorithms/solved/insertion.js
@@ -34,8 +34,8 @@ function insertionSort (arr) {
 
   for (let i = 1; i < arr.length; i++) {
 
-    for (let j = i; j >= 0; j--) {
-      
+    for (let j = i; j > 0; j--) {
+
       if (arr[j] < arr[j - 1]) {
         const temp = arr[j - 1]
         arr[j - 1] = arr[j]
